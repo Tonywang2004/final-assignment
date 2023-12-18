1c072ffb8bbd97924289d87a150f6e1275fea162
Sheerio <2900952121@qq.com>
2023-12-18 20:35:21 +0800

    优化后的调用小小英雄移动示例

--- ApplyLittleHero.cpp
+++ ApplyLittleHero.cpp
@@ -1,5 +1,6 @@
 #include "ApplyLittleHero.h"
 #include "MoveLittleHero.h"
+#include "cocos2d.h"
 USING_NS_CC;
 
 Scene* ApplyLittleHero::createScene()
@@ -30,7 +31,7 @@ bool  ApplyLittleHero::init()
         backgroundSprite->setScale(scaleX, scaleY);
 
         // 将背景精灵添加到场景中
-        this->addChild(backgroundSprite, 0); 
+        this->addChild(backgroundSprite, 0);
     }
 
 
@@ -38,48 +39,15 @@ bool  ApplyLittleHero::init()
     /*以下为小小英雄移动部分*/
 
     // 创建小小英雄
-    auto hero = littleHero::create("LittlePlayer_1_0.png"); 
+    auto hero = littleHero::create("LittlePlayer_4_0.png");
     if (hero) {
         hero->setPosition(Vec2(visibleSize.width / 2.8 + origin.x, visibleSize.height / 2.8 + origin.y)); // 设置英雄的位置
         hero->setScale(0.5);
         this->addChild(hero); // 将英雄添加到场景中
     }
-    //加载小小英雄的动作帧图片,该步必不可少
-    preloadWalkingFrames();
-
-    // 在对局开始,启用小小英雄移动
     hero->enableMouseControl(true);
-
     // 在游戏准备阶段，关闭小小英雄移动
     //hero->enableMouseControl(false);
- 
 
     return true;
-}
-
-//加载行走动作帧，如果要增加小小精灵，注意该函数
-void  ApplyLittleHero::preloadWalkingFrames() {
-    // 写这段代码时的屏幕分辨率
-    const float designWidth = 2560.0f;
-    const float designHeight = 1440.0f;
-
-    // 获取当前视窗大小
-    auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
-
-    // 计算缩放因子
-    float scaleX = visibleSize.width / designWidth;
-    float scaleY = visibleSize.height / designHeight;
-
-    // 当前帧图片的尺寸
-    float originalFrameWidth = 400.0f;
-    float originalFrameHeight = 320.0f;
-
-    // 调整帧尺寸以适应当前分辨率
-    float frameWidth = originalFrameWidth * scaleX;
-    float frameHeight = originalFrameHeight * scaleY;
-    for (int i = 1; i <= 8; ++i) {
-        std::string frameName = "LittlePlayer_1_" + std::to_string(i) + ".png"; // 加载帧图片
-        auto frame = cocos2d::SpriteFrame::create(frameName, cocos2d::Rect(0, 0, frameWidth, frameHeight));
-        cocos2d::SpriteFrameCache::getInstance()->addSpriteFrame(frame, frameName);
-    }
 }
